b5a799a18d173dae9cfcdbe4483c12ccdfc5babf
josefaidt <josef.aidt@gmail.com>
2022-07-29 08:57:45 -0500

    wip

---
+++ src/app.ts
@@ -0,0 +1,52 @@
+import 'source-map-support/register.js'
+import { fileURLToPath } from 'node:url'
+import * as cdk from 'aws-cdk-lib'
+import { Runtime } from 'aws-cdk-lib/aws-lambda'
+import * as lambda from 'aws-cdk-lib/aws-lambda-nodejs'
+import { Construct } from 'constructs'
+import { GitHubActions } from './github-actions'
+import { pkg } from './pkg'
+
+const app = new cdk.App({
+  context: {
+    name: 'fakerepo',
+    env: 'main',
+    version: pkg.version,
+  },
+})
+
+export class Stack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props)
+
+    cdk.Tags.of(this).add('app:name', this.node.tryGetContext('name'))
+    cdk.Tags.of(this).add('app:env', this.node.tryGetContext('env'))
+    cdk.Tags.of(this).add('app:version', this.node.tryGetContext('version'))
+
+    new GitHubActions(this, 'GitHubActions', {
+      repo: 'josefaidt/fakerepo',
+    })
+
+    new lambda.NodejsFunction(this, 'MyFunction', {
+      entry: fileURLToPath(new URL('./handler.ts', import.meta.url)),
+      functionName: 'my-function',
+      bundling: {
+        minify: true, // minify code, defaults to false
+        sourceMap: true, // include source map, defaults to false
+        sourceMapMode: lambda.SourceMapMode.INLINE, // defaults to SourceMapMode.DEFAULT
+        target: 'esnext',
+        define: {
+          'import.meta.vitest': 'undefined',
+          'process.env.NODE_ENV': JSON.stringify('production'),
+        },
+        tsconfig: fileURLToPath(new URL('../tsconfig.json', import.meta.url)),
+        format: lambda.OutputFormat.ESM,
+      },
+      runtime: Runtime.NODEJS_16_X,
+    })
+  }
+}
+
+new Stack(app, 'Stack', {
+  stackName: `fakerepo-main`,
+})
--- t.sh
+++
@@ -1,4 +0,0 @@
-url="https://github.com/josefaidt/fakerepo/releases/tag/v0.0.6-next.24"
-tag=$(echo "${url/releases\/tag/tree}")
-
-echo "tag is $tag"
